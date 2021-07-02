1b4635daf8392d5fbf93190e369e98de40fa6ae5
Dwayne <dwaynecharrington@gmail.com>
2021-07-02 14:38:26 +1000

    feat(plugin): remove existing cruft from plugin import

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,8 @@
 import { DI, IContainer, IRegistry } from '@aurelia/kernel';
 
-export const DefaultComponents: IRegistry[] = [
-    AureliaTableCustomAttribute as unknown as IRegistry,
-    AutSortCustomAttribute as unknown as IRegistry,
-    AutSelectCustomAttribute as unknown as IRegistry,
-    AutPaginationCustomElement as unknown as IRegistry,
-];
-
-const aureliaTableConfiguration = {
-    register(container: IContainer): IContainer {
-        return container.register(
-            ...DefaultComponents
-        );
-    },
-
-    createContainer(): IContainer {
-        return this.register(DI.createContainer());
-    }
-};
-
-export const AureliaTableConfiguration = {
-    customize(components: any[] = []) {
-        return { ...aureliaTableConfiguration };
-    },
-    ...aureliaTableConfiguration
-};
-
 const components: IRegistry[] = [
-
+    // MyComponent as unknown as IRegistry,
 ];
-
 
 export const PluginConfiguration = {
     register(container: IContainer): IContainer {
